d6f804868ebb435cec147de8a82bd276683f9d80
jankun4 <michaljankun@gmail.com>
2022-04-25 16:29:56 +0200

    vulkan

--- desktop/nvidia.nix
+++ desktop/nvidia.nix
@@ -3,6 +3,8 @@
   nixpkgs.config.allowUnfree = true;
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+  hardware.opengl.driSupport.enable = true;
+  environment.systemPackages = with pkgs; [ vulkan-tools vulkan-loader ];
 }
 
 
