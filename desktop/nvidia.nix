b23827a23ad843d4a8a15a7e3eeda654eac75d4b
jankun4 <michaljankun@gmail.com>
2022-04-25 17:46:48 +0200

    wayland

--- desktop/nvidia.nix
+++ desktop/nvidia.nix
@@ -5,6 +5,8 @@
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
   hardware.opengl.driSupport.enable = true;
   environment.systemPackages = with pkgs; [ vulkan-tools vulkan-loader ];
+  services.xserver.displayManager.gdm.nvidiaWayland = true;
+  hardware.nvidia.modesetting.enable = true;
 }
 
 
--- desktop/xserver.nix
+++ desktop/xserver.nix
@@ -14,6 +14,7 @@
   environment.systemPackages = with pkgs; [
     firefox
     gnomeExtensions.appindicator
+    gnome.gnome-tweaks
   ];
 
   services.udev.packages = with pkgs; [
